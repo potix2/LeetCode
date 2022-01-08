540d5b1b42a43f8ee117353b5317c7ddd7a769cd
Katsunori Kanda <potix2@gmail..com>
2022-01-08 15:35:21 +0900

    Solved PathSum

--- explore/learn/card/data-structure-tree/17/solve-problems-recursively/537/main.go
+++ explore/learn/card/data-structure-tree/17/solve-problems-recursively/537/main.go
@@ -1,1 +1,26 @@
-package _37
+package explore
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) ||
+		hasPathSum(root.Right, targetSum-root.Val)
+}
