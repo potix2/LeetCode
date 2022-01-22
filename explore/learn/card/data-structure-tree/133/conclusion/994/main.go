318ded8fdc09c25ab010dc3f70c5726cd6286a1a
Katsunori Kanda <potix2@gmail.com>
2022-01-22 18:32:45 +0900

    Solved: Populating Next Right Pointers in Each Node

---
+++ explore/learn/card/data-structure-tree/133/conclusion/994/main.go
@@ -0,0 +1,37 @@
+package explore
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	leftmost := root
+	for leftmost.Left != nil {
+		head := leftmost
+		for head != nil {
+			head.Left.Next = head.Right
+			if head.Next != nil {
+				head.Right.Next = head.Next.Left
+			}
+			head = head.Next
+		}
+		leftmost = leftmost.Left
+	}
+	return root
+}
